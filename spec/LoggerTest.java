9b7b977d9cfc8eb456bd0bcd80c9f7ff36b0234f
Mike Knepper <michaelrknepper@gmail.com>
2014-05-07 12:12:23 -0500

    Adds Logger class to print to system out

---
+++ spec/LoggerTest.java
@@ -0,0 +1,30 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class LoggerTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    @Before
+    public void setUpOutput() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void cleanUpOutput() {
+        System.setOut(null);
+    }
+
+
+    @Test
+    public void itLogsAMessageToSystemOut() {
+        Logger logger = new Logger();
+        logger.log("Hello world");
+
+        assertEquals("Hello world\n", output.toString());
+    }
+}
---
+++ src/Logger.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mrk on 5/7/14.
+ */
+public class Logger {
+    public void log(String message) {
+        System.out.println(message);
+    }
+}
