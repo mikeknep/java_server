81e4f58e14d88d80ec52254988a64b40c65f6e9b
Mike Knepper <michaelrknepper@gmail.com>
2014-05-07 11:27:29 -0500

    Listener collects an incoming request

--- spec/ListenerTest.java
+++ spec/ListenerTest.java
@@ -1,25 +1,21 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class ListenerTest {
 
     @Test
-    public void itSetsAPort() {
+    public void itCollectsARequest() throws Exception {
         Listener listener = new Listener();
-        listener.setPort(8399);
+        ByteArrayInputStream incoming = new ByteArrayInputStream("Hello world".getBytes());
 
-        assertEquals(8399, listener.port);
-    }
+        ArrayList<String> requestCollection = new ArrayList<String>();
+        requestCollection.add("Hello world");
 
-    @Test
-    public void itCreatesAServerSocketOnItsPort() throws Exception {
-        Listener listener = new Listener();
-        listener.setPort(8399);
-        listener.createServerSocket();
-
-        assertEquals(ServerSocket.class, listener.serverSocket.getClass());
+        assertEquals(requestCollection, listener.collect(incoming));
     }
 }
--- src/Listener.java
+++ src/Listener.java
@@ -1,23 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by mrk on 5/6/14.
  */
 public class Listener {
-    int port;
-    ServerSocket serverSocket;
-
-    public void setPort(int portNumber) {
-        this.port = portNumber;
-    }
-
-    public void createServerSocket() throws Exception {
-        this.serverSocket = new ServerSocket(this.port);
-    }
 
     public Socket listen(int portNumber) throws Exception {
         ServerSocket serverSocket = new ServerSocket(portNumber);
         return serverSocket.accept();
+    }
+
+    public ArrayList<String> collect(InputStream incoming) throws Exception {
+        InputStreamReader isr = new InputStreamReader(incoming);
+        BufferedReader br = new BufferedReader(isr);
+
+        ArrayList<String> requestCollection = new ArrayList<String>();
+
+        requestCollection.add(br.readLine());
+
+        return requestCollection;
     }
 }
