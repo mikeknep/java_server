c13e9a6c408dac3e931933b3f5381be3f89629c5
Mike Knepper <michaelrknepper@gmail.com>
2014-05-13 14:07:49 -0500

    Deletes old Responder class; Runner uses ResourceHandler

--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,12 @@
-import java.io.File;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
 /**
 * Created by mrk on 5/7/14.
 */
 public class Runner {
     Listener listener = new Listener();
-    Responder responder = new Responder();
     Logger logger = new Logger();
 
     public static void main(String[] args) throws Exception
@@ -27,12 +25,16 @@ public class Runner {
             String rawRequest = listener.collectRawRequest(socket.getInputStream());
             Request request = new Request(rawRequest);
 
-            File resource = responder.locateFile("/Users/mrk/Desktop/dahomey", request.getResource());
-            String body = responder.readFile(resource);
+            ResourceHandler handler = new ResourceHandler("/Users/mrk/Desktop/dahomey", request.getResource());
+            Response response = new Response(request.getVersion(), handler.getStatus(), handler.getResourceString());
 
-            Response response = new Response(request.getVersion(), "200 OK", body);
             String responseAsString = ResponsePresenter.present(response);
-            responder.respond(responseAsString, socket.getOutputStream());
+
+
+            // This needs to be wrapped in some object
+            PrintWriter outWriter = new PrintWriter(socket.getOutputStream(), true);
+            outWriter.println(responseAsString);
+
 
             socket.close();
         }
