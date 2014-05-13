3e852f43fa0529a0547f4d46c71ceda6a9db5f3f
Mike Knepper <michaelrknepper@gmail.com>
2014-05-13 12:01:51 -0500

    ResourceLocator can identify if requested resources are present in given directory

---
+++ spec/ResourceLocatorTest.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class ResourceLocatorTest {
+    @Test
+    public void recognizesPresentResourceWithRelativePath() {
+        assertEquals(true, ResourceLocator.resourceIsPresent("spec", "/mock.html"));
+    }
+
+    @Test
+    public void recognizesMissingResourceWithRelativePath() {
+        assertEquals(false, ResourceLocator.resourceIsPresent("spec", "/nonexistent.html"));
+    }
+
+    @Test
+     public void recognizesPresentResourceWithAbsolutePath() {
+        assertEquals(true, ResourceLocator.resourceIsPresent("/Users/mrk/8thlight/dahomey/spec", "/mock.html"));
+    }
+
+    @Test
+    public void recognizesMissingResourceWithAbsolutePath() {
+        assertEquals(false, ResourceLocator.resourceIsPresent("/Users/mrk/8thlight/dahomey/spec", "/nonexistent.html"));
+    }
+
+    @Test
+    public void understandsRelativeAndAbsolutePathsToSameFile() throws Exception {
+        Path relative = Paths.get("spec/mock.html");
+        Path absolute = Paths.get("/Users/mrk/8thlight/dahomey/spec/mock.html");
+        assertEquals(true, Files.isSameFile(relative, absolute));
+    }
+}
---
+++ src/ResourceLocator.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by mrk on 5/13/14.
+ */
+public class ResourceLocator {
+    public static boolean resourceIsPresent(String directory, String requestedResource) {
+        Path path = Paths.get(directory + requestedResource);
+        return Files.exists(path);
+    }
+}
