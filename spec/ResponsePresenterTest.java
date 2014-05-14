ef204c4477e6d017b11491c9928e8799291356ea
Mike Knepper <michaelrknepper@gmail.com>
2014-05-14 15:25:28 -0500

    Updates ResponsePresenterTest per new Response constructor

--- spec/ResponsePresenterTest.java
+++ spec/ResponsePresenterTest.java
@@ -1,17 +1,25 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ResponsePresenterTest {
-//    Response response = new Response("HTTP/1.1", "200 OK", "Hello world");
-//
-//    @Test
-//    public void itGeneratesAStatusLine() {
-//        assertEquals("HTTP/1.1 200 OK", ResponsePresenter.generateStatusLine(response));
-//    }
-//
-//    @Test
-//    public void itPresentsAResponseAsASingleString() {
-//        assertEquals("HTTP/1.1 200 OK\n\nHello world", ResponsePresenter.present(response));
-//    }
+    Response response;// = new Response();//"HTTP/1.1", "200 OK", "Hello world");
+    @Before
+    public void setUpResponse() {
+        response = new Response();
+        response.setVersion("HTTP/1.1");
+        response.setStatus("200 OK");
+        response.setBody("Hello world");
+    }
+
+    @Test
+    public void itGeneratesAStatusLine() {
+        assertEquals("HTTP/1.1 200 OK", ResponsePresenter.generateStatusLine(response));
+    }
+
+    @Test
+    public void itPresentsAResponseAsASingleString() {
+        assertEquals("HTTP/1.1 200 OK\n\nHello world", ResponsePresenter.present(response));
+    }
 }
