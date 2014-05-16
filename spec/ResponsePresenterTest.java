6c4085c209a80b82b8a9cd88a73d901baaeae697
Mike Knepper <michaelrknepper@gmail.com>
2014-05-16 11:13:34 -0500

    ResponsePresenter generates a bytearray, not string

--- spec/ResponsePresenterTest.java
+++ spec/ResponsePresenterTest.java
@@ -11,15 +11,24 @@ public class ResponsePresenterTest {
         response.setVersion("HTTP/1.1");
         response.setStatus("200 OK");
         response.setBody("Hello world");
+        response.setBodyData("Hello world".getBytes());
+        response.setHeader("Content-Length", "135");
+        response.setHeader("Content-Type", "text/html");
     }
 
     @Test
     public void itGeneratesAStatusLine() {
-        assertEquals("HTTP/1.1 200 OK", ResponsePresenter.generateStatusLine(response));
+        assertEquals("HTTP/1.1 200 OK\n", ResponsePresenter.generateStatusLine(response));
     }
 
     @Test
-    public void itPresentsAResponseAsASingleString() {
-        assertEquals("HTTP/1.1 200 OK\n\nHello world", ResponsePresenter.generateFullResponse(response));
+    public void itGeneratesHeadersAsStrings() {
+        assertEquals("Content-Length: 135\nContent-Type: text/html\n", ResponsePresenter.generateHeaders(response));
+    }
+
+    @Test
+    public void itGeneratesFullResponseByteArray() {
+        byte[] fullResponseByteArray = "HTTP/1.1 200 OK\nContent-Length: 135\nContent-Type: text/html\n\nHello world".getBytes();
+        assertEquals(fullResponseByteArray, ResponsePresenter.generateFullResponseByteArray(response));
     }
 }
--- src/ResponsePresenter.java
+++ src/ResponsePresenter.java
@@ -1,19 +1,49 @@
+import java.util.Map;
+
 /**
  * Created by mrk on 5/13/14.
  */
 public class ResponsePresenter {
-    public static String generateFullResponse(Response response) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(generateStatusLine(response));
-        builder.append("\n");
-        builder.append("\n");
-        builder.append(response.getBody());
+    public static byte[] generateFullResponseByteArray(Response response) {
+        // determine size of full byte[] response and initialize
+        int size = generateStatusLine(response).getBytes().length
+                   + generateHeaders(response).getBytes().length
+                   + "\n".getBytes().length
+                   + response.getBodyData().length;
+        byte[] responseByteArray = new byte[size];
 
-        return builder.toString();
+        // add status line
+        for (int i = 0; i < generateStatusLine(response).getBytes().length; i++) {
+            responseByteArray[i] = generateStatusLine(response).getBytes()[i];
+        }
+        // add headers
+        for (int i = 0; i < generateHeaders(response).getBytes().length; i++) {
+            responseByteArray[generateStatusLine(response).getBytes().length + i] = generateHeaders(response).getBytes()[i];
+        }
+        // add blank line
+        for (int i = 0; i < "\n".getBytes().length; i++) {
+            responseByteArray[generateStatusLine(response).getBytes().length + generateHeaders(response).getBytes().length + i] = "\n".getBytes()[i];
+        }
+        // add body
+        for (int i = 0; i < response.getBodyData().length; i++) {
+            responseByteArray[generateStatusLine(response).getBytes().length + generateHeaders(response).getBytes().length + "\n".getBytes().length + i] = response.getBodyData()[i];
+        }
+
+        // return byte array
+        return responseByteArray;
     }
 
-
     public static String generateStatusLine(Response response) {
-        return (response.getVersion() + " " + response.getStatus());
+        return (response.getVersion() + " " + response.getStatus() + "\n");
+    }
+
+    public static String generateHeaders(Response response) {
+        StringBuilder builder = new StringBuilder();
+
+        for(Map.Entry<String, String> header : response.getHeaders().entrySet()) {
+            builder.append(header.getKey() + ": " + header.getValue() + "\n");
+        }
+
+        return builder.toString();
     }
 }
