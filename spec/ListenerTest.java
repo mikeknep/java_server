111cf4c917981b90f2dc3816ce45c1941890fcaa
Mike Knepper <michaelrknepper@gmail.com>
2014-05-19 16:26:08 -0500

    Adds before/after to spec for Logger

--- spec/ListenerTest.java
+++ spec/ListenerTest.java
@@ -1,14 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class ListenerTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    @Before
+    public void setUpOutput() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @After
+    public void cleanUpOutput() {
+        System.setOut(null);
+    }
+
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
