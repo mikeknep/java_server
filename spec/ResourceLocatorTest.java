0de2eb5e0379244d33761a00e57bf6e60161305c
Mike Knepper <michaelrknepper@gmail.com>
2014-05-19 11:04:56 -0500

    Refactors ResourceLocatorTest

--- spec/ResourceLocatorTest.java
+++ spec/ResourceLocatorTest.java
@@ -8,29 +8,12 @@ import static org.junit.Assert.*;
 
 public class ResourceLocatorTest {
     @Test
-    public void recognizesPresentResourceWithRelativePath() {
-        assertEquals(true, ResourceLocator.resourceIsPresent("spec", "/sample_files/mock.html"));
+    public void recognizesPresentResource() {
+        assertTrue(ResourceLocator.resourceIsPresent("spec/sample_files", "/mock.html"));
     }
 
     @Test
-    public void recognizesMissingResourceWithRelativePath() {
-        assertEquals(false, ResourceLocator.resourceIsPresent("spec", "/nonexistent.html"));
-    }
-
-    @Test
-     public void recognizesPresentResourceWithAbsolutePath() {
-        assertEquals(true, ResourceLocator.resourceIsPresent("/Users/mrk/8thlight/dahomey/spec", "/sample_files/mock.html"));
-    }
-
-    @Test
-    public void recognizesMissingResourceWithAbsolutePath() {
-        assertEquals(false, ResourceLocator.resourceIsPresent("/Users/mrk/8thlight/dahomey/spec", "/nonexistent.html"));
-    }
-
-    @Test
-    public void understandsRelativeAndAbsolutePathsToSameFile() throws Exception {
-        Path relative = Paths.get("spec/sample_files/mock.html");
-        Path absolute = Paths.get("/Users/mrk/8thlight/dahomey/spec/sample_files/mock.html");
-        assertEquals(true, Files.isSameFile(relative, absolute));
+    public void recognizesMissingResource() {
+        assertFalse(ResourceLocator.resourceIsPresent("spec/sample_files", "/nonexistent.html"));
     }
 }
