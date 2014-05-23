959ecd44f79ec84deadd41580355c3c45ca541e2
Mike Knepper <michaelrknepper@gmail.com>
2014-05-23 09:21:24 -0500

    SocketStreamPair tracks time of socket opening

--- src/SocketStreamPair.java
+++ src/SocketStreamPair.java
@@ -2,15 +2,18 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * Created by mrk on 5/19/14.
  */
 public class SocketStreamPair implements StreamPair {
     private Socket socket;
+    private Date socketOpenTime;
 
     public SocketStreamPair(ServerSocket serverSocket) throws Exception {
         this.socket = serverSocket.accept();
+        this.socketOpenTime = new Date();
     }
 
     public InputStream getIn() throws Exception {
@@ -19,6 +22,10 @@ public class SocketStreamPair implements StreamPair {
 
     public OutputStream getOut() throws Exception {
         return this.socket.getOutputStream();
+    }
+
+    public Date getSocketOpenTime() {
+        return this.socketOpenTime;
     }
 
     public void close() throws Exception {
