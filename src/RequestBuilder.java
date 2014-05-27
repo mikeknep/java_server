3aa22379b9550c2da4b8902d2cea52e13a2a7b37
Mike Knepper <michaelrknepper@gmail.com>
2014-05-27 17:38:54 -0500

    RequestBuilder can build invalid request
    Invalid requests, such as "phantom" requests, should not break the
    high-level functionality of the server (i.e. accept request and generate
    response). The RequestBuilder can therefore build an invalid Request
    object deliberately, knowing that something further down the line will
    understand how to handle an invalid request (namely, generate a 400
    response).

--- src/RequestBuilder.java
+++ src/RequestBuilder.java
@@ -5,40 +5,46 @@ import java.util.HashMap;
  */
 public class RequestBuilder {
     public static Request buildRequest(String rawRequest) throws Exception {
-        String requestLine = parseRequestLine(rawRequest);
-        String method = parseRequestMethod(requestLine);
-        String resource = parseRequestResource(requestLine);
-        String version = parseRequestVersion(requestLine);
-        HashMap<String, String> headers = parseRequestHeaders(rawRequest);
+        try {
+            String requestLine = parseRequestLine(rawRequest);
+            String method = parseMethod(requestLine);
+            String resource = parseResource(requestLine);
+            String version = parseVersion(requestLine);
+            HashMap<String, String> headers = parseHeaders(rawRequest);
 
-        return new Request(method, resource, version, headers);
+            return new Request(method, resource, version, headers);
+        } catch (Exception e) {
+            return new Request("", "", "", new HashMap<String, String>());
+        }
     }
 
     private static String parseRequestLine(String rawRequest) {
         return rawRequest.split("\n", 2)[0];
     }
 
-    private static String parseRequestMethod(String requestLine) {
+    private static String parseMethod(String requestLine) {
         return requestLine.split(" ")[0];
     }
 
-    private static String parseRequestResource(String requestLine) {
+    private static String parseResource(String requestLine) {
         return requestLine.split(" ")[1];
     }
 
-    private static String parseRequestVersion(String requestLine) {
+    private static String parseVersion(String requestLine) {
         return requestLine.split(" ")[2];
     }
 
-    private static HashMap<String, String> parseRequestHeaders(String rawRequest) {
-        String rawHeaders = rawRequest.split("\n", 2)[1];
-        String[] allHeaders = rawHeaders.split("\n");
+    private static HashMap<String, String> parseHeaders(String rawRequest) {
         HashMap<String, String> headers = new HashMap<String, String>();
+        try {
+            String rawHeaders = rawRequest.split("\n", 2)[1];
+            String[] allHeaders = rawHeaders.split("\n");
 
-        for(String header : allHeaders) {
-            String[] kvPair = header.split(": ", 2);
-            headers.put(kvPair[0], kvPair[1]);
-        }
+            for (String header : allHeaders) {
+                String[] kvPair = header.split(": ", 2);
+                headers.put(kvPair[0], kvPair[1]);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {}
 
         return headers;
     }
