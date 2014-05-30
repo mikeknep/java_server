a285a4879e8be4fe4d55d31d3be4f8293af478b6
Mike Knepper <michaelrknepper@gmail.com>
2014-05-30 11:02:14 -0500

    Removes unused Logger methods

--- src/Logger.java
+++ src/Logger.java
@@ -4,25 +4,9 @@ import java.util.Date;
  * Created by mrk on 5/7/14.
  */
 public class Logger {
-    public static void log(String message) {
-        System.out.println(message);
-    }
 
     public static void logStartup(int port, String directory) {
         System.out.println("Server initialized on port " + port + ", serving directory " + directory);
-    }
-
-    public static void logException(Exception exception, String responseType) {
-        String serverResponse;
-        if (responseType.equals("ignore")) {
-            serverResponse = "Ignored.";
-        } else if (responseType.equals("500")) {
-            serverResponse = "500 response sent.";
-        } else {
-            serverResponse = "";
-        }
-
-        System.out.println("Exception caught: " + exception + ". " + serverResponse);
     }
 
     public static void logBasic(Request request, Response response, Date timeOfRequest) {
--- test/LoggerTest.java
+++ test/LoggerTest.java
@@ -23,31 +23,10 @@ public class LoggerTest {
 
 
     @Test
-    public void itLogsAMessageToSystemOut() {
-        Logger.log("Hello world");
-
-        assertEquals("Hello world\n", output.toString());
-    }
-
-    @Test
     public void itLogsAStartupMessage() {
         Logger.logStartup(8399, "/Users/mrk/Desktop");
 
         assertTrue(output.toString().contains("8399") && output.toString().contains("Desktop"));
-    }
-
-    @Test
-    public void itLogsIgnoredException() {
-        Logger.logException(new Exception(), "ignore");
-
-        assertTrue(output.toString().contains("Ignored"));
-    }
-
-    @Test
-    public void itLogs500Exception() {
-        Logger.logException(new Exception(), "500");
-
-        assertTrue(output.toString().contains("500"));
     }
 
     @Test
