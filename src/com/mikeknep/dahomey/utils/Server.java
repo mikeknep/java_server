a0af44e1038faa7e7201ee5f233261fceea1fa57
Mike Knepper <michaelrknepper@gmail.com>
2014-06-04 11:21:40 -0500

    Spikes in a threadpool
    Server class uses an ExecutorService to spin off threads. The code that
    gets called inside each thread should probably be moved to a separate
    class ("Worker"?).

--- src/com/mikeknep/dahomey/utils/Server.java
+++ src/com/mikeknep/dahomey/utils/Server.java
@@ -8,6 +8,8 @@ import com.mikeknep.dahomey.responses.Response;
 import com.mikeknep.dahomey.responses.ResponseFactory;
 
 import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
 * Created by mrk on 5/7/14.
@@ -16,6 +18,7 @@ public class Server {
     private String directory;
     private ServerSocket serverSocket;
     private String application;
+    private ExecutorService threadPool = Executors.newFixedThreadPool(16);
 
     public Server(String directory, ServerSocket serverSocket, String application) throws Exception {
         this.directory = directory;
@@ -29,15 +32,24 @@ public class Server {
 
     public void run() throws Exception {
         while (true) {
-            SocketConnection clientConnection = new SocketConnection(serverSocket);
-            String rawRequest = Listener.receiveRawRequest(clientConnection.getIn());
-            Request request = RequestBuilder.buildRequest(rawRequest);
-            ApplicationInteractor interactor = new ApplicationInteractor(directory, request, application);
-            interactor.runApplication();
-            Response response = ResponseFactory.buildResponse(interactor.getStatus(), interactor.getHeaders(), interactor.getBody());
-            Responder.sendResponse(response, clientConnection.getOut());
-            Logger.logBasic(request, response, clientConnection.getSocketOpenTime());
-            clientConnection.close();
+            final SocketConnection clientConnection = new SocketConnection(serverSocket);
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String rawRequest = Listener.receiveRawRequest(clientConnection.getIn());
+                        Request request = RequestBuilder.buildRequest(rawRequest);
+                        ApplicationInteractor interactor = new ApplicationInteractor(directory, request, application);
+                        interactor.runApplication();
+                        Response response = ResponseFactory.buildResponse(interactor.getStatus(), interactor.getHeaders(), interactor.getBody());
+                        Responder.sendResponse(response, clientConnection.getOut());
+                        Logger.logBasic(request, response, clientConnection.getSocketOpenTime());
+                        clientConnection.close();
+                    } catch (Exception exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            });
         }
     }
 }
