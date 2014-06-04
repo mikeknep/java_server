429d671f2b7313322f8bb6f7f41e63a041b7a29e
Mike Knepper <michaelrknepper@gmail.com>
2014-06-04 15:48:06 -0500

    Refactors ResponsePresenter method
    Simplifies the ResponsePresenter's present method by using Java's
    System.arraycopy method.

--- src/com/mikeknep/dahomey/responses/ResponsePresenter.java
+++ src/com/mikeknep/dahomey/responses/ResponsePresenter.java
@@ -7,73 +7,35 @@ import java.util.Map;
  */
 public class ResponsePresenter {
     public static byte[] present(Response response) {
-        int size = statusSize(response) + headersSize(response) + newlineSize() + bodySize(response);
-        byte[] responseByteArray = new byte[size];
+        byte[] status = formatStatusLine(response);
+        byte[] headers = formatHeaders(response);
+        byte[] newline = "\n".getBytes();
+        byte[] body = response.getBody();
 
-        for (int i = 0; i < statusSize(response); i++) {
-            responseByteArray[i] = statusBytes(response)[i];
-        }
-        for (int i = 0; i < headersSize(response); i++) {
-            int offset = statusSize(response);
-            responseByteArray[offset + i] = headersBytes(response)[i];
-        }
-        for (int i = 0; i < newlineSize(); i++) {
-            int offset = statusSize(response) + headersSize(response);
-            responseByteArray[offset + i] = newlineBytes()[i];
-        }
-        for (int i = 0; i < bodySize(response); i++) {
-            int offset = statusSize(response) + headersSize(response) + newlineSize();
-            responseByteArray[offset + i] = bodyBytes(response)[i];
-        }
+        int size = status.length + headers.length + newline.length + body.length;
+        byte[] fullResponse = new byte[size];
 
-        return responseByteArray;
+        System.arraycopy(status, 0, fullResponse, 0, status.length);
+        System.arraycopy(headers, 0, fullResponse, status.length, headers.length);
+        System.arraycopy(newline, 0, fullResponse, status.length + headers.length, newline.length);
+        System.arraycopy(body, 0, fullResponse, status.length + headers.length + newline.length, body.length);
+
+        return fullResponse;
     }
 
 
 
-    private static String formatStatusLine(Response response) {
-        return ("HTTP/1.1 " + response.getStatus() + "\n");
+    private static byte[] formatStatusLine(Response response) {
+        return ("HTTP/1.1 " + response.getStatus() + "\n").getBytes();
     }
 
-    private static String formatHeaders(Response response) {
+    private static byte[] formatHeaders(Response response) {
         StringBuilder builder = new StringBuilder();
 
         for(Map.Entry<String, String> header : response.getHeaders().entrySet()) {
             builder.append(header.getKey() + ": " + header.getValue() + "\n");
         }
 
-        return builder.toString();
-    }
-
-    private static byte[] statusBytes(Response response) {
-        return formatStatusLine(response).getBytes();
-    }
-
-    private static byte[] headersBytes(Response response) {
-        return formatHeaders(response).getBytes();
-    }
-
-    private static byte[] newlineBytes() {
-        return "\n".getBytes();
-    }
-
-    private static byte[] bodyBytes(Response response) {
-        return response.getBody();
-    }
-
-    private static int statusSize(Response response) {
-        return statusBytes(response).length;
-    }
-
-    private static int headersSize(Response response) {
-        return headersBytes(response).length;
-    }
-
-    private static int newlineSize() {
-        return newlineBytes().length;
-    }
-
-    private static int bodySize(Response response) {
-        return bodyBytes(response).length;
+        return builder.toString().getBytes();
     }
 }
