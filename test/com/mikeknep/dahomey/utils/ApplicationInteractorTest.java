beb65a28a9aa1b78290cec0bb6e7a3652f0376af
Mike Knepper <michaelrknepper@gmail.com>
2014-06-12 16:20:52 -0500

    Adds high level AppInteractor tests

--- test/com/mikeknep/dahomey/utils/ApplicationInteractorTest.java
+++ test/com/mikeknep/dahomey/utils/ApplicationInteractorTest.java
@@ -1,22 +1,55 @@
 package com.mikeknep.dahomey.utils;
 
+import com.mikeknep.dahomey.requests.Request;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class ApplicationInteractorTest {
-    @Test
-    public void itReturnsStatusFromApplication() {
-        //
+    ApplicationInteractor badAppInteractor;
+    ApplicationInteractor goodAppInteractor;
+    @Before
+    public void instantiateInteractors() throws Exception {
+        Request request = new Request("GET", "/nothere.html", new HashMap<String, String>(), "");
+        badAppInteractor = new ApplicationInteractor("public/", request, "nothere.jar");
+        goodAppInteractor = new ApplicationInteractor("public/", request, "public/mock.jar");
+        badAppInteractor.runApplication();
+        goodAppInteractor.runApplication();
     }
 
     @Test
-    public void itReturnsHeadersFromApplication() {
-        //
+    public void itReturns500StatusFromBadApp() {
+        assertEquals("500 Internal Server Error", badAppInteractor.getStatus());
     }
 
     @Test
-    public void itReturnsBodyFromApplication() {
-        //
+    public void itReturnsStatusFromGoodApp() {
+        assertEquals("404 Not Found", goodAppInteractor.getStatus());
+    }
+
+    @Test
+    public void itReturnsBlankHeadersFromBadApp() {
+        assertEquals(new HashMap<String, String>(), badAppInteractor.getHeaders());
+    }
+
+    @Test
+    public void itReturnsHeadersFromGoodApp() {
+        HashMap<String, String> expectedHeaders = new HashMap<String, String>();
+        expectedHeaders.put("Content-Type", "text/plain");
+
+        assertEquals(expectedHeaders, goodAppInteractor.getHeaders());
+    }
+
+    @Test
+    public void itReturnsBlankBodyFromBadApp() {
+        assertArrayEquals("500".getBytes(), badAppInteractor.getBody());
+    }
+
+    @Test
+    public void itReturnsBodyFromGoodApp() {
+        assertArrayEquals("404".getBytes(), goodAppInteractor.getBody());
     }
 }
